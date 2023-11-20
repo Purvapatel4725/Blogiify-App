bc10058eae5ccf575ab3d73863d07995595601b9
Purvam Patel <43619996+purvampatel@users.noreply.github.com>
2023-11-20 12:22:03 -0500

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ let DB = require('../config/db');
 let userModel = require('../models/user');
 let User = userModel.User;
 
+//Login Setup
 router.get('/login', function(req,res,net){
   if(!req.user)
   {
@@ -13,6 +14,44 @@ router.get('/login', function(req,res,net){
     {
       title:'Login',
       message: req.flash('loginMessage'),
+      displayName: req.user? req.user.displayName: ""
+    }
+    )
+  }
+  else{
+    return res.redirect('/')
+  }
+})
+
+router.post('/login',function(req,res,net){
+  passport.authenticate('local',function(err,User,info)
+  {
+    //server error
+    if(err){
+      return next(err);
+    }
+    //login error
+    if(!User){
+      req.flash('loginMessage','AuthenticationError')
+    }
+    req.login(User,(err)=>{
+      if(err)
+      {
+        return next(err)
+      }
+      return res.redirect('/blog');
+    })
+  })(req,res,next)
+})
+
+
+router.get('/register', function(req,res,next){
+  if(!req.user)
+  {
+    res.render('auth/register',
+    {
+      title:'Register',
+      message: req.flash('registerMessage'),
       displayName: req.user? req.user.displayName: ""
     }
     )
