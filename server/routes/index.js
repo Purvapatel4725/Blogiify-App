139c36dc537ed3197c42376a010ce546162875ff
Purvam Patel <43619996+purvampatel@users.noreply.github.com>
2023-11-20 12:08:41 -0500

    Update index.js for login page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const passport = require('passport');
+let DB = require('../config/db');
+let userModel = require('../models/user');
+let User = userModel.User;
+
+router.get('/login', function(req,res,net){
+  if(!req.user)
+  {
+    res.render('auth/login',
+    {
+      title:'Login',
+      message: req.flash('loginMessage'),
+      displayName: req.user? req.user.displayName: ""
+    }
+    )
+  }
+  else{
+    return res.redirect('/')
+  }
+})
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
