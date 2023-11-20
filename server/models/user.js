13079d5bc0c013d719270800522842799624124c
Purvam Patel <43619996+purvampatel@users.noreply.github.com>
2023-11-20 11:51:27 -0500

    Created user.js

---
+++ server/models/user.js
@@ -0,0 +1,49 @@
+let mongoose = require('mongoose');
+let passportLocalMongoose = require('passport-local-mongoose');
+
+
+let User = mongoose.Schema({
+    username:
+    {
+        type:String,
+        default:"",
+        trim:true,
+        required:'Username is required'
+    },
+    /*
+    password:
+    {
+        type:String,
+        default:"",
+        trim:true,
+        required:'Password is required'
+    },
+    */
+   displayName:
+   {
+        type:String,
+        default:"",
+        trim:true,
+        required:'Display Name is required'
+   },
+   created:
+   {
+        type:Date,
+        default: Date.now
+   },
+   update:
+   {
+        type:Date,
+        default: Date.now
+   }
+},
+{
+    collection:"user"
+}
+)
+
+//configure model
+let options = ({MissingPasswordError:'Wrong/Missing Password'})
+User.plugin(passportLocalMongoose,options);
+
+module.exports.User = mongoose.model('User',User);
