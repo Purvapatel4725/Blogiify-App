7ef5a456a79763aa5f5d8318aa4a2c5a8933b9ff
Purvam Patel <patelpurvam007@gmail.com>
2023-11-23 16:00:49 -0500

    Login updates

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -2,11 +2,20 @@ const express = require('express');
 const router = express.Router();
 const BlogController = require('../controllers/BlogController');
 
+// helper function
+function requireAuth(req,res,next){
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login')
+    }
+    next();
+}
+
 router.get('/', BlogController.DisplayBlogPostList);
-router.get('/add', BlogController.AddBlogPost);
-router.post('/add', BlogController.ProcessBlogPost);
-router.get('/edit/:id', BlogController.EditBlogPost);
-router.post('/edit/:id', BlogController.ProcessEditBlogPost);
-router.get('/delete/:id', BlogController.DeleteBlogPost);
+router.get('/add', requireAuth, BlogController.AddBlogPost);
+router.post('/add', requireAuth, BlogController.ProcessBlogPost);
+router.get('/edit/:id', requireAuth, BlogController.EditBlogPost);
+router.post('/edit/:id', requireAuth, BlogController.ProcessEditBlogPost);
+router.get('/delete/:id', requireAuth, BlogController.DeleteBlogPost);
 
 module.exports = router;
