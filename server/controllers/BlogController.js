0993da135efa933e6f72567dd311fc922a0ba229
chinmayaramani <144928321+chinmayaramani@users.noreply.github.com>
2023-12-02 22:01:05 +0000

    added commenting

--- server/controllers/BlogController.js
+++ server/controllers/BlogController.js
@@ -1,27 +1,35 @@
+// Importing the BlogPost model
 const BlogPost = require('../models/BlogPost');
 
+// BlogController object with various methods
 const BlogController = {
+    // Method to display a list of blog posts
     DisplayBlogPostList: async (req, res, next) => {
         try {
+            // Fetch all blog posts from the database
             const blogPosts = await BlogPost.find();
+            
+            // Render the 'blog/list' view and pass data to it
             res.render('blog/list', {
                 title: 'Blogs',
                 blogPosts: blogPosts,
-                displayName: req.user ? req.user.displayName:''
+                displayName: req.user ? req.user.displayName : ''
             });
         } catch (err) {
             console.error(err);
+            // Render the 'blog/list' view with an error message in case of an error
             res.render('blog/list', {
                 error: 'Error on server'
             });
         }
     },
 
+    // Method to render the 'add blog' page
     AddBlogPost: async (req, res, next) => {
         try {
             res.render('blog/add', {
                 title: 'Add Blog',
-                displayName: req.user ? req.user.displayName:''
+                displayName: req.user ? req.user.displayName : ''
             });
         } catch (err) {
             console.error(err);
@@ -31,13 +39,17 @@ const BlogController = {
         }
     },
 
+    // Method to process the addition of a new blog post
     ProcessBlogPost: async (req, res, next) => {
         try {
+            // Create a new BlogPost instance with data from the request
             const newBlogPost = BlogPost({
                 "title": req.body.title,
                 "content": req.body.content,
             });
+            // Save the new blog post to the database
             await newBlogPost.save();
+            // Redirect to the blog post list page
             res.redirect('/blog');
         } catch (error) {
             console.error(error);
@@ -47,18 +59,24 @@ const BlogController = {
         }
     },
 
+    // Method to render the 'edit blog' page
     EditBlogPost: async (req, res, next) => {
         try {
             const postId = req.params.id;
+            // Find the blog post by ID
             const blogPost = await BlogPost.findById(postId);
+            
+            // Check if the blog post exists
             if (!blogPost) {
                 res.render('error', { error: 'Blog post not found!' });
                 return;
             }
+            
+            // Render the 'blog/edit' view with the data of the blog post
             res.render('blog/edit', {
                 title: 'Edit Blog',
                 Blog: blogPost,
-                displayName: req.user ? req.user.displayName:''
+                displayName: req.user ? req.user.displayName : ''
             });
         } catch (error) {
             console.error(error);
@@ -68,14 +86,18 @@ const BlogController = {
         }
     },
 
+    // Method to process the editing of a blog post
     ProcessEditBlogPost: async (req, res, next) => {
         try {
             const postId = req.params.id;
+            // Updated data for the blog post
             const updatedPostData = {
                 title: req.body.title,
                 content: req.body.content,
             };
+            // Find the blog post by ID and update its data
             await BlogPost.findByIdAndUpdate(postId, updatedPostData);
+            // Redirect to the blog post list page
             res.redirect('/blog');
         } catch (error) {
             console.error(error);
@@ -85,10 +107,13 @@ const BlogController = {
         }
     },
 
+    // Method to delete a blog post
     DeleteBlogPost: async (req, res, next) => {
         try {
             const postId = req.params.id;
+            // Find the blog post by ID and delete it
             await BlogPost.findByIdAndDelete(postId);
+            // Redirect to the blog post list page
             res.redirect('/blog');
         } catch (error) {
             console.error(error);
@@ -99,4 +124,5 @@ const BlogController = {
     },
 };
 
+// Export the BlogController object
 module.exports = BlogController;
--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,3 +1,4 @@
+// Importing required modules and dependencies
 let express = require('express');
 let router = express.Router();
 
@@ -6,97 +7,104 @@ let DB = require('../config/db');
 let userModel = require('../models/user');
 let User = userModel.User;
 
-module.exports.GetLogin = (req,res,next) => {
-    if(!req.user)
-    {
-      res.render('auth/login',
-      {
-        title:'Login',
-        message: req.flash('loginMessage'),
-        displayName: req.user ? req.user.displayName:''
-      })
+// Route handler for rendering the login page
+module.exports.GetLogin = (req, res, next) => {
+    // Check if the user is not already logged in
+    if (!req.user) {
+        res.render('auth/login', {
+            title: 'Login',
+            message: req.flash('loginMessage'), // Flash messages for login errors
+            displayName: req.user ? req.user.displayName : ''
+        });
+    } else {
+        // If the user is already logged in, redirect to the home page
+        return res.redirect('/');
     }
-    else{
-      return res.redirect('/')
-    }
-  }
+}
 
-module.exports.PostLogin = (req,res,next) => {
-    passport.authenticate('local',function(err,User,info){
-        // server error
-        if(err)
-        {
-          return next(err);
+// Route handler for processing login
+module.exports.PostLogin = (req, res, next) => {
+    // Authenticate the user using Passport local strategy
+    passport.authenticate('local', function (err, user, info) {
+        // Handle server error
+        if (err) {
+            return next(err);
         }
-        // login error
-        if(!User)
-        {
-          req.flash('loginMessage',
-          'AuthenticationError');
-          return res.redirect('/login')
+        // Handle authentication error
+        if (!user) {
+            req.flash('loginMessage', 'Authentication Error'); // Flash message for login error
+            return res.redirect('/login');
         }
-        req.login(User,(err)=>{
-          if(err)
-          {
-            return next(err)
-          }
-          return res.redirect('/blog');
-        })
-    })(req,res,next)
-  }
+        // Log in the user using Passport login method
+        req.login(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            // Redirect to the blog page after successful login
+            return res.redirect('/blog');
+        });
+    })(req, res, next);
+}
 
-module.exports.GetRegister = (req,res,next) => {
-    if(!req.user)
-    {
-      res.render('auth/register',
-      {
-        title:'Register',
-        message: req.flash('registerMessage'),
-        displayName: req.user ? req.user.displayName: ''
-      })
+// Route handler for rendering the registration page
+module.exports.GetRegister = (req, res, next) => {
+    // Check if the user is not already logged in
+    if (!req.user) {
+        res.render('auth/register', {
+            title: 'Register',
+            message: req.flash('registerMessage'), // Flash messages for registration errors
+            displayName: req.user ? req.user.displayName : ''
+        });
+    } else {
+        // If the user is already logged in, redirect to the home page
+        return res.redirect('/');
     }
-    else{
-      return res.redirect('/')
-    }
-  }
+}
 
-  module.exports.PostRegister = (req,res,next) => {
+// Route handler for processing registration
+module.exports.PostRegister = (req, res, next) => {
+    // Create a new User instance with registration data
     let newUser = new User({
-      username: req.body.username,
-      // password: req.body.password,
-      email: req.body.email,
-      displayName: req.body.displayName
-    })
-    User.register(newUser, req.body.password,(err) => {
-      if(err)
-      {
-        console.log("Error in inserting new User");
-        if(err.name =='UserExistError')
-        {
-          req.flash('registerMessage',
-          'Registration Error : User already Exist'
-        )}
-        return res.render('auth/register',
-        {
-          title:'Register',
-          message: req.flash('registerMessage'),
-          displayName: req.user ? req.user.displayName:''
-        })
-      }
-      else{
-        return passport.authenticate('local')(req,res,()=>{
-          res.redirect('/blog');
-        })
-      }
-    })
-  }
+        username: req.body.username,
+        // password: req.body.password, // Note: Password is not directly provided here
+        email: req.body.email,
+        displayName: req.body.displayName
+    });
 
-module.exports.GetLogout = (req,res,next) => {
-    req.logout(function(err){
-      if(err)
-      {
-        return next(err);
-      }
-    })
-    res.redirect('/')
-  }
+    // Register the new user using the User model's register method
+    User.register(newUser, req.body.password, (err) => {
+        if (err) {
+            console.log("Error in inserting new User");
+
+            // Handle registration error (user already exists)
+            if (err.name == 'UserExistError') {
+                req.flash('registerMessage', 'Registration Error: User already exists');
+            }
+
+            // Render the registration page with an error message
+            return res.render('auth/register', {
+                title: 'Register',
+                message: req.flash('registerMessage'),
+                displayName: req.user ? req.user.displayName : ''
+            });
+        } else {
+            // Authenticate the user after successful registration
+            return passport.authenticate('local')(req, res, () => {
+                // Redirect to the blog page after successful registration and login
+                res.redirect('/blog');
+            });
+        }
+    });
+}
+
+// Route handler for logging out the user
+module.exports.GetLogout = (req, res, next) => {
+    // Use Passport's logout method to log out the user
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+    });
+    // Redirect to the home page after logout
+    res.redirect('/');
+}
