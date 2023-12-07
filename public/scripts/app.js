c5b5ec37c50bf458b29dad3add13215fddd28258
Purvam Patel <patelpurvam007@gmail.com>
2023-12-07 14:33:54 -0500

    Script location updated

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,21 @@ function confirmDelete(blogPostId) {
         window.location.href = '/blog/delete/' + blogPostId;
     }
 }
+
+ // Function to hide the logo on mobile screens
+ function hideLogoOnMobile() {
+    const logo = document.querySelector('.img-hide-custom'); // Select the logo element
+
+    // Check if the screen width is less than or equal to 767px (typical mobile screen size)
+    if (window.innerWidth <= 767) {
+      logo.style.display = 'none'; // Hide the logo
+    } else {
+      logo.style.display = 'block'; // Show the logo for larger screens
+    }
+  }
+
+  // Initial call to hide the logo based on the screen size on page load
+  window.addEventListener('load', hideLogoOnMobile);
+
+  // Listener for window resize events to dynamically update logo visibility
+  window.addEventListener('resize', hideLogoOnMobile);
