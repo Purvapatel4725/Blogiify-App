a77564f90274dc67c431e308bc8197b29ef80d6a
Purva Patel <144143336+Purvapatel4725@users.noreply.github.com>
2024-09-26 16:15:50 -0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,1 @@
-module.exports = {"URI":"mongodb+srv://Username:PASSWORD@clusternull0.lsxh2cn.mongodb.net/blogDB"}
+module.exports = {"URI":"mongodb+srv://USERNAME:PASSWORD@clusternull0.lsxh2cn.mongodb.net/blogDB"}
